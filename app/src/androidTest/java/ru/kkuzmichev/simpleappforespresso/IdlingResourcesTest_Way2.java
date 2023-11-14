e6e32350c3d87db76e296f855c05cca293f3e2c5
pm-sh <pmshuv975@gmail.com>
2023-11-15 00:05:34 +0300

    IdlingResourcesTest changed

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/IdlingResourcesTest_Way2.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/IdlingResourcesTest_Way2.java
@@ -3,17 +3,19 @@ package ru.kkuzmichev.simpleappforespresso;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.core.AllOf.allOf;
+import static org.hamcrest.CoreMatchers.allOf;
 import static ru.kkuzmichev.simpleappforespresso.CustomViewAssertions.isRecycleView;
 import static ru.kkuzmichev.simpleappforespresso.CustomViewMatcher.recyclerViewSizeMatcher;
 
+import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.test.espresso.IdlingRegistry;
 import androidx.test.espresso.ViewInteraction;
-import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
 import org.junit.After;
@@ -22,23 +24,14 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import io.qameta.allure.android.runners.AllureAndroidJUnit4;
-
 @LargeTest
-//@RunWith(AndroidJUnit4.class)
-@RunWith(AllureAndroidJUnit4.class)
+@RunWith(AndroidJUnit4.class)
+//@RunWith(AllureAndroidJUnit4.class)
 
-public class IdlingResourcesTest {
-
-    private ViewInteraction appCompatImageButton = onView(withContentDescription("Open navigation drawer"));
-    private ViewInteraction checkedTextGallery = onView(withId(R.id.nav_gallery));
-    private ViewInteraction itemSeventh = onView(allOf(withId(R.id.item_number), withText("7")));
-    private ViewInteraction recycleListing = onView(withId(R.id.recycle_view));
-
+public class IdlingResourcesTest_Way2 {
     @Rule
-//    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
-//            new ActivityScenarioRule<>(MainActivity.class);
-    public IntentsTestRule<MainActivity> activityTestRule = new IntentsTestRule<>(MainActivity.class);
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
 
     @Before
     public void registerIdlingResources() {
@@ -52,30 +45,45 @@ public class IdlingResourcesTest {
 
     @Test
     public void idlingResourcesTest() {
+        ViewInteraction appCompatImageButton = onView(isAssignableFrom(AppCompatImageButton.class));
         appCompatImageButton.check(matches(isDisplayed()));
         appCompatImageButton.perform(click());
-        checkedTextGallery.check(matches(isDisplayed()));
-        checkedTextGallery.perform(click());
-        itemSeventh.check(matches(isDisplayed()));
-        itemSeventh.check(matches(withText("7")));
+
+        ViewInteraction navigationMenuItemView = onView(withId(R.id.nav_gallery));
+        navigationMenuItemView.check(matches(isDisplayed()));
+        navigationMenuItemView.perform(click());
+
+        ViewInteraction sevenItem = onView(allOf(withId(R.id.item_number), withText("7")));
+        sevenItem.check(matches(isDisplayed()));
+        sevenItem.check(matches(withText("7")));
     }
 
     @Test
     public void checkingNumberElementsTest() {
+        ViewInteraction appCompatImageButton = onView(isAssignableFrom(AppCompatImageButton.class));
         appCompatImageButton.check(matches(isDisplayed()));
         appCompatImageButton.perform(click());
+
+        ViewInteraction checkedTextGallery = onView(withId(R.id.nav_gallery));
         checkedTextGallery.check(matches(isDisplayed()));
         checkedTextGallery.perform(click());
+
+        ViewInteraction recycleListing = onView(withId(R.id.recycle_view));
         recycleListing.check(matches(isDisplayed()));
         recycleListing.check(matches(recyclerViewSizeMatcher(10)));
     }
 
     @Test
     public void checkingThatListIsListTest() {
+        ViewInteraction appCompatImageButton = onView(isAssignableFrom(AppCompatImageButton.class));
         appCompatImageButton.check(matches(isDisplayed()));
         appCompatImageButton.perform(click());
+
+        ViewInteraction checkedTextGallery = onView(withId(R.id.nav_gallery));
         checkedTextGallery.check(matches(isDisplayed()));
         checkedTextGallery.perform(click());
+
+        ViewInteraction recycleListing = onView(withId(R.id.recycle_view));
         recycleListing.check(isRecycleView());
     }
 }
